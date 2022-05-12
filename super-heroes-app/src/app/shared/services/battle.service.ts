e809d8888b1b5891835764d57ddea1afc0eabcc9
Maxim Zmeyok <maxim_zmeyok@mail.ru>
2022-05-12 11:28:14 +0300

    refactor: made constants for types of filter in sort method and added types for values

--- super-heroes-app/src/app/shared/services/battle.service.ts
+++ super-heroes-app/src/app/shared/services/battle.service.ts
@@ -6,23 +6,28 @@ export class BattleService {
   public battlesHistory: BattleResult[] = [];
 
   public sortBattlesHistory(filter: string): void {
+    const date: string = 'date';
+    const heroName: string = 'heroName';
+    const enemyName: string = 'enemyName';
+    const result: string = 'result';
+
     switch(filter) {
-      case 'date':
-        this.battlesHistory.sort((a, b) => a.date - b.date);
+      case date:
+        this.battlesHistory.sort((a: BattleResult, b: BattleResult): number => a.date - b.date);
         break;
-      case 'heroName':
-        this.battlesHistory.sort((a, b) => this._compareStrings(a.heroName, b.heroName));
+      case heroName:
+        this.battlesHistory.sort((a: BattleResult, b: BattleResult): number => this._compareStrings(a.heroName, b.heroName));
         break;
-      case 'enemyName':
-        this.battlesHistory.sort((a, b) => this._compareStrings(a.enemyName, b.enemyName));
+      case enemyName:
+        this.battlesHistory.sort((a: BattleResult, b: BattleResult): number => this._compareStrings(a.enemyName, b.enemyName));
         break;
-      case 'result':
-        this.battlesHistory.sort((a, b) => this._compareStrings(a.result, b.result));
+      case result:
+        this.battlesHistory.sort((a: BattleResult, b: BattleResult): number => this._compareStrings(a.result, b.result));
         break;
     }
   }
 
-  private _compareStrings(a: string, b: string) {
+  private _compareStrings(a: string, b: string): number {
     return a.localeCompare(b);
   }
 
