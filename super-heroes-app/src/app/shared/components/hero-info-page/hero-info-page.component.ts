d6c3de7799c3f9ae09c07bab042c4c44c9961a56
Maxim Zmeyok <maxim_zmeyok@mail.ru>
2022-05-13 13:13:47 +0300

    feat: made the ability to watch a page with a certain hero information

--- super-heroes-app/src/app/shared/components/hero-info-page/hero-info-page.component.ts
+++ super-heroes-app/src/app/shared/components/hero-info-page/hero-info-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Observable, switchMap } from 'rxjs';
+import { Hero } from '../../interfaces';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-hero-info-page',
@@ -6,10 +10,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero-info-page.component.sass']
 })
 export class HeroInfoPageComponent implements OnInit {
+  public hero$: Observable<Hero>;
 
-  constructor() { }
+  constructor(
+    private _route: ActivatedRoute,
+    private _heroesService: HeroesService
+  ) { }
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
+    this._initHero();
+  }
+
+  private _initHero(): void {
+    this.hero$ = this._route.params
+    .pipe(switchMap((params: Params) => {
+      return this._heroesService.getHeroById(params['id']);
+    }));
   }
 
 }
